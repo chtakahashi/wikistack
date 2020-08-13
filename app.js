51615f9ffb76a0d5688772d884f4ecaba13aa483
Xander Bakx <xanderbakx@gmail.com>
2020-08-13 15:14:09 -0400

    synced models & additional validation options

--- app.js
+++ app.js
@@ -1,31 +1,45 @@
-const express = require('express')
-const morgan = require('morgan')
-const { db } = require('./models')
-const bodyParser = require('body-parser')
+const express = require("express");
+const morgan = require("morgan");
+const { db, Page, User } = require("./models");
+const bodyParser = require("body-parser");
 const {
   addPage,
   editPage,
   main,
   userList,
   userPages,
-  wikiPage
-} = require('./views/index')
+  wikiPage,
+} = require("./views/index");
 
-db.authenticate()
-  .then(() => {
-    console.log('connected to the database');
-  })
+db.authenticate().then(() => {
+  console.log("connected to the database");
+});
 
-const app = express()
+const app = express();
 
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({
-  extended: false
-}))
-app.use(morgan('dev'))
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan("dev"));
 
-app.get('/', (req, res) => {
-  res.send(main(''))
-})
+app.get("/", (req, res) => {
+  res.send(main(""));
+});
 
-app.listen(3000)
+const sync = async () => {
+  try {
+    await Page.sync({
+      force: true,
+    });
+    await User.sync({
+      force: true,
+    });
+    await db.close();
+  } catch (error) {
+    await db.close();
+    console.log(error);
+  }
+};
+
+sync();
+
+app.listen(3000);
--- models/index.js
+++ models/index.js
@@ -1,16 +1,18 @@
-const Sequelize = require('sequelize')
-const db = new Sequelize('postgres://localhost:5432/wikistack');
+const Sequelize = require('sequelize');
+const db = new Sequelize('postgres://localhost:5432/wikistack', {
+  logging: false,
+});
 
 const Page = db.define('page', {
-  title: Sequelize.STRING,
-  slug: Sequelize.STRING,
-  content: Sequelize.TEXT,
-  status: Sequelize.ENUM('open', 'closed')
-})
+  title: { type: Sequelize.STRING, allowNull: false },
+  slug: { type: Sequelize.STRING, allowNull: false },
+  content: { type: Sequelize.TEXT, allowNull: false },
+  status: Sequelize.ENUM('open', 'closed'),
+});
 
 const User = db.define('user', {
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-})
+  name: { type: Sequelize.STRING, allowNull: false },
+  email: { type: Sequelize.STRING, allowNull: false, unique: true },
+});
 
-module.exports = { db, Page, User }
+module.exports = { db, Page, User };
